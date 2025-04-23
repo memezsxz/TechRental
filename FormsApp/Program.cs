6dd9cd59b5ac5660b16506c1c6db478cfc863e62
memezsxz <109390466+memezsxz@users.noreply.github.com>
2025-04-24 02:30:00 +0300

    removed S3 call from Program.cs

--- FormsApp/Program.cs
+++ FormsApp/Program.cs
@@ -1,53 +1,78 @@
 ﻿using Database.Persistence;
 using System.Windows.Forms;
 using System.Threading.Tasks;
+using Amazon.S3.Model;
+using System.Windows.Forms;
 
 namespace FormsApp
 {
     internal static class Program
     {
+
+        #region Main Run
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static async Task Main()
+        static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-
-            using (var context = new RentalDBContext())
-            {
-                try
-                {
-                    //var bb = new List<byte> { 1, 2, 3 };
-                    //var x = S3Manager.DownloadBinaryAsync("bush.jpg").Result;
-                    //var y = S3Manager.UploadBinaryAsync(bb.ToArray(), "test").Result;
-
-                    //uplad image to S3
-                    Console.WriteLine("🚀 Starting S3 Upload...");
-                    await S3Uploader.UploadImagesAsync();
-                    Console.WriteLine("✅ Upload process completed.");
-
-                    //Retrive image from S3
-                    //Image img = await S3Uploader.GetImageByGuidAsync("0e78757b-8ad9-4837-8a29-645f7706f0c3");
-
-                    //if (img != null)
-                    //{
-                    //    img.Save(@"C:\Users\Ruqay\Downloads\fetched.png", System.Drawing.Imaging.ImageFormat.Png);
-                    //    Console.WriteLine("✅ Image saved as fetched.png");
-                    //}
-
-                    context.Database.EnsureCreated();
-                    Console.WriteLine("Database connection successful!");
-
-                    ApplicationConfiguration.Initialize();
-                    Application.Run(new Home());
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
-            }
+            ApplicationConfiguration.Initialize(); 
+            Application.Run(new Home());
         }
+
+
+        #endregion
+
+        #region S3 Testing
+        ///// <summary>
+        /////  The main entry point for the application.
+        ///// </summary>
+        //[STAThread]
+        //static async Task Main()
+        //{
+        //    // To customize application configuration such as set high DPI settings or default font,
+        //    // see https://aka.ms/applicationconfiguration.
+
+        //    using (var context = new RentalDBContext())
+        //    {
+        //        try
+        //        {
+        //            //var bb = new List<byte> { 1, 2, 3 };
+        //            //var x = S3Manager.DownloadBinaryAsync("bush.jpg").Result;
+        //            //var y = S3Manager.UploadBinaryAsync(bb.ToArray(), "test").Result;
+
+        //            //uplad image to S3
+        //            Console.WriteLine("🚀 Starting S3 Upload...");
+        //            await S3Uploader.UploadImagesAsync();
+        //            Console.WriteLine("✅ Upload process completed.");
+
+        //            //Retrive image from S3
+        //            //Image img = await S3Uploader.GetImageByGuidAsync("0e78757b-8ad9-4837-8a29-645f7706f0c3");
+
+        //            //if (img != null)
+        //            //{
+        //            //    img.Save(@"C:\Users\Ruqay\Downloads\fetched.png", System.Drawing.Imaging.ImageFormat.Png);
+        //            //    Console.WriteLine("✅ Image saved as fetched.png");
+        //            //}
+
+        //            context.Database.EnsureCreated();
+        //            Console.WriteLine("Database connection successful!");
+
+        //            ApplicationConfiguration.Initialize();
+        //            Application.Run(new Home());
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine($"Error: {ex.Message}");
+        //        }
+        //    }
+        //}
+
+
+        #endregion
+
     }
 }
