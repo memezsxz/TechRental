1d31adc71b752d541a34b5e8ba1067bffc807747
dbsaw <hussain.sabbai@gmail.com>
2025-05-04 12:22:37 +0300

    added audit log and error log, modified the equipment controller and the layoutpage

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -9,11 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddDbContext<RentalDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
