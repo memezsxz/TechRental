5fd668ccb7c9a41b6afe9361aa93a2653181d84c
FatimaAbdali <ffooffy35@gmail.com>
2025-05-14 15:01:32 +0300

    link the dashboard

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 
@@ -7,14 +9,29 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Contains("Admin") || roles.Contains("Manager"))
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+            }
+
             return View();
         }
 
